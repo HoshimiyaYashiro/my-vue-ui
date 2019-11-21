27c12ba1b9252d0251d53302a04d1f4218f2fc53
KhuongNQ <khuong.nguyenquoc@nttdata.com>
2019-11-21 18:05:46 +0700

    Init Project

---
+++ src/views/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about">
+    <UiSelect :options="select.options" :result="select.result" />
+  </div>
+</template>
+
+<style lang="sass">
+@import 'src/assets/style/ui-style.sass'
+</style>
+
+<script>
+import UiSelect from "@/components/UiSelect.vue";
+
+export default {
+  data() {
+    return {
+      select: {
+        result: null,
+        options: [
+          { value: "0", name: "Fate Grand Order" },
+          { value: "1", name: "Onmyoji" },
+          { value: "2", name: "Sword Art Online" },
+          { value: "3", name: "Date A Live" },
+          { value: "4", name: "Code Gear" },
+          { value: "5", name: "Guilty Crown" }
+        ]
+      }
+    };
+  },
+  methods: {
+  },
+  components: {
+    UiSelect
+  }
+};
+</script>
