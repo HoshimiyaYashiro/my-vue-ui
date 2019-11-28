ad165165bab20d9b21dd9a3f99fb8c17a1a5669c
KhuongNQ <khuong.nguyenquoc@nttdata.com>
2019-11-28 18:12:30 +0700

    Finish Scroll Component

--- src/components/UiScroll.vue
+++ src/components/UiScroll.vue
@@ -3,19 +3,20 @@
     <div class="ui-scroll-wrap" @scroll="scrollHandle($event)" ref="scrollWrap">
       <slot></slot>
     </div>
-    <div class="ui-scrollbar-bar is-vertical" ref="verticalScroll" :class="{active: isMouseDown}">
-      <div
-        class="ui-scrollbar-thump"
-        :style="{height: scrollYPercentage + '%', transform: 'translateY(' + translateY + '%)'}"
-        @mousedown="mouseDownHandle"
+    <div class="ui-scrollbar-bar is-vertical" ref="verticalScroll" :class="{active: isMouseDown}" @click.self.prevent="clickTrackHandle($event, 'vertical')">
+      <div class="ui-scrollbar-thump"
+        :style="{height: vertical.percentage + '%', transform: 'translateY(' + vertical.move + '%)'}"
+        @mousedown="mouseDownHandle($event, 'vertical')" ref="verticalThump"
       ></div>
     </div>
-    <div class="ui-scrollbar-bar is-horizontal" ref="horizontalScroll">
-      <div class="ui-scrollbar-thump"></div>
+    <div class="ui-scrollbar-bar is-horizontal" ref="horizontalScroll" @click.self.prevent="clickTrackHandle($event, 'horizontal')">
+      <div class="ui-scrollbar-thump" 
+      :style="{width: horizontal.percentage + '%', transform: 'translateX(' + horizontal.move + '%)'}" 
+      @mousedown="mouseDownHandle($event, 'horizontal')" ref="horizontalThump"></div>
     </div>
   </div>
 </template>
-
+,
 <script>
 export default {
     name: 'UiScroll',
@@ -23,58 +24,93 @@ export default {
     components: {},
     data() {
         return {
-            scrollYPercentage: 0,
-            scrollXPercentage: 0,
-            translateY: 0,
-            translateX: 0,
-            scrollY: 0,
-            scrollX: 0,
-            isMouseDown : false,
-            axisY: 0
+          vertical: {
+            percentage: 0,
+            move: 0,
+            scrollByPx: 0,
+            point: 0,
+            offset: 'offsetHeight',
+            client: 'clientY',
+            direction: 'top',
+            scroll: 'scrollTop',
+            scrollSize: 'scrollHeight',
+            thump: 'verticalThump',
+            track: 'verticalScroll'
+          },
+          horizontal: {
+            percentage: 0,
+            move: 0,
+            scrollByPx: 0,
+            point: 0,
+            offset: 'offsetWidth',
+            client: 'clientX',
+            direction: 'left',
+            scroll: 'scrollLeft',
+            scrollSize: 'scrollWidth',
+            thump: 'horizontalThump',
+            track: 'horizontalScroll'
+          },
+          isMouseDown : false,
+          bar: null
         }
     },
     methods: {
         scrollHandle() {
             const $el = this.$refs.scrollWrap;
-            if (this.scrollX != $el.scrollLeft) {
-                this.scrollX = $el.scrollLeft;
-                this.translateX = Math.round(($el.scrollLeft / $el.clientWidth * 100) * 1000) / 1000;
+            if (this.horizontal.scrollByPx != $el.scrollLeft) {
+                this.horizontal.scrollByPx = $el.scrollLeft;
+                this.horizontal.move = Math.round(($el.scrollLeft / $el.clientWidth * 100) * 1000) / 1000;
             }
-            if (this.scrollY != $el.scrollTop) {
-                this.scrollY = $el.scrollLeft;
-                this.translateY = Math.round(($el.scrollTop / $el.clientHeight * 100) * 1000) / 1000;
-                console.log($el.scrollTop);
+            if (this.vertical.scrollByPx != $el.scrollTop) {
+                this.vertical.scrollByPx = $el.scrollTop;
+                this.vertical.move = Math.round(($el.scrollTop / $el.clientHeight * 100) * 1000) / 1000;
             }
         },
         mouseMoveHandle(e) {
-          if (this.isMouseDown) {
-            const $el = this.$refs.scrollWrap;
-            const offset = ((this.$refs.verticalScroll.getBoundingClientRect().top - e.clientY) * -1);
-            const thumbClickPosition = (e.target.offsetHeight - this.axisY);
-            const thumbPositionPercentage = ((offset - thumbClickPosition) * 100 / this.$el.offsetHeight);
-            const scrollTop = Math.round((thumbPositionPercentage * this.scrollYPercentage / 100) * 1000) / 1000;
-            this.$refs.scrollWrap.scrollTop = scrollTop;
-            console.log(scrollTop);
-          }
+          if (!this.isMouseDown) return;
+          if (!this.bar.point) return;
+          const $thump = this.$refs[this.bar.thump];
+          const $track = this.$refs[this.bar.track];
+          const $wrap = this.$refs.scrollWrap;
+          const offset = (($track.getBoundingClientRect()[this.bar.direction] - e[this.bar.client]) * -1);
+          const thumbClickPosition = ($thump[this.bar.offset] - this.bar.point);
+          const thumbPositionPercentage = ((offset - thumbClickPosition) * 100 / $track[this.bar.offset]);
+          const scroll = Math.round((thumbPositionPercentage * $wrap[this.bar.scrollSize] / 100) * 1000) / 1000;
+          $wrap[this.bar.scroll] = scroll;
         },
         mouseUpHandle(e) {
           this.isMouseDown = false;
-          this.axisY = 0;
+          this.bar.point = 0;
+          document.removeEventListener('mousemove', this.mouseMoveHandle);
+          document.onselectstart = null;
         },
-        mouseDownHandle(e) {
-          e.stopImmediatePropagation();
+        mouseDownHandle(e, bar) {
+          if (e.ctrlKey || e.button === 2) return;
+          this.bar = this[bar];
           this.isMouseDown = true;
-          this.axisY = (e.currentTarget.offsetHeight - (e.clientY - e.currentTarget.getBoundingClientRect().top));
           document.addEventListener('mousemove', this.mouseMoveHandle);
           document.addEventListener('mouseup', this.mouseUpHandle);
           document.onselectstart = () => false;
+          this.bar.point = (e.currentTarget[this.bar.offset] - (e[this.bar.client] - e.currentTarget.getBoundingClientRect()[this.bar.direction]));
+        },
+        clickTrackHandle(e, bar) {
+          this.bar = this[bar];
+          const $wrap = this.$refs.scrollWrap;
+          const $thump = this.$refs[this.bar.thump];
+          const offset = Math.abs(e.target.getBoundingClientRect()[this.bar.direction] - e[this.bar.client]);
+          const thumbHalf = ($thump[this.bar.offset] / 2);
+          const thumbPositionPercentage = ((offset - thumbHalf) * 100 / e.target[this.bar.offset]);
+          $wrap[this.bar.scroll] = (thumbPositionPercentage * $wrap[this.bar.scrollSize] / 100);
+        },
+        initScrollPercentage() {
+          const $el = this.$refs.scrollWrap;
+          this.vertical.percentage = Math.round(($el.clientHeight / $el.scrollHeight * 100) * 1000) / 1000;
+          this.horizontal.percentage = Math.round(($el.clientWidth / $el.scrollWidth * 100) * 1000) / 1000;
         }
     },
     mounted() {
-        const self = this;
-        const $el = self.$refs.scrollWrap;
-        self.scrollYPercentage = Math.round(($el.clientHeight / $el.scrollHeight * 100) * 1000) / 1000;
-        self.scrollXPercentage = Math.round(($el.clientWidth / $el.scrollWidth * 100) * 1000) / 1000;
+      const self = this;
+      self.initScrollPercentage();
     },
     destroyed() {
       document.removeEventListener('mouseup', this.mouseUpHandle);
--- src/components/UiSelect.vue
+++ src/components/UiSelect.vue
@@ -1,11 +1,13 @@
 <template name="ui-select">
   <div class="ui-select">
-    <input type="text" class="ui-input-inner" placeholder="Select an option" readonly />
-    <UiScroll :parent-class="'ui-dropdown-inner'">
-      <ul class="ui-dropdown-group">
-        <li v-for="option in options" :key="option.value">{{option.name}}</li>
-      </ul>
-    </UiScroll>
+    <input type="text" class="ui-input-inner" placeholder="Select an option" readonly @click="openDropdown = !openDropdown"/>
+    <transition name="slide" :after-enter="afterEnter">
+      <UiScroll :parent-class="'ui-dropdown-inner'" v-if="openDropdown">
+        <ul class="ui-dropdown-group">
+          <li v-for="option in options" :key="option.value">{{option.name}}</li>
+        </ul>
+      </UiScroll>
+    </transition>
   </div>
 </template>
 
@@ -16,9 +18,15 @@ export default {
   name: "UiSelect",
   props: ["options", "result"],
   data() {
-    return {};
+    return {
+      openDropdown: false
+    };
   },
-  methods: {},
+  methods: {
+    afterEnter: function (el) {
+      console.log(this);
+    }
+  },
   components: {
     UiScroll
   },
